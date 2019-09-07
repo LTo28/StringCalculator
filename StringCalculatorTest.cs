c45961ed5372af9ca4317b323a990733005b52e1
lto28 <to.larry92@gmail.com>
2019-09-07 14:05:01 -0700

    add 2 numbers working

--- StringCalculatorTest.cs
+++ StringCalculatorTest.cs
@@ -17,7 +17,18 @@ namespace Main
         }
         [TestCase("1",1)]
         [TestCase("20",20)]
+        [TestCase("5000",5000)]
         public void Add_Single_ReturnThatNumber(string number, int expected)
+        {
+            StringCalculator calculator = new StringCalculator();
+
+            int results = calculator.Add(number);
+
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("1,5000", 5001)]
+        [TestCase("1,1",2)]
+        public void Add_MultipleNumbers_ReturnSum(string number, int expected)
         {
             StringCalculator calculator = new StringCalculator();
 
@@ -30,9 +41,15 @@ namespace Main
     {
        public int Add(string number)
        {
+            String[] multiNumbers = number.Split(',');
+
             if (number.Length == 0)
             {
                 return 0;
+            }
+            else if (number.Contains(","))
+            {
+                return int.Parse(multiNumbers[0]) + int.Parse(multiNumbers[1]);
             }
             return int.Parse(number);
        }
