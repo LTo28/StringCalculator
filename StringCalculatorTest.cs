f97acf6a338d75809a7c69653a2846e740250c69
lto28 <to.larry92@gmail.com>
2019-09-09 19:30:30 -0700

    working requirement 6

--- StringCalculatorTest.cs
+++ StringCalculatorTest.cs
@@ -9,19 +9,16 @@ namespace Main
         StringCalculator calculator = new StringCalculator();
 
         [TestCase("",0)]
-        [TestCase("1", 1)]
         [TestCase("20", 20)]
-        [TestCase("5000", 5000)]
         [TestCase("tytyt", 0)]
-        [TestCase("a", 0)]
         [TestCase("1,100", 101)]
-        [TestCase("1,1",2)]
         [TestCase("5,tytyt",5)]
         [TestCase("1,2,3,4,5,6,7,8,9,10,11,12",78)]
         [TestCase("1\n2,3",6)]
-        [TestCase("10,-5",5)]
         [TestCase("2,1000,6",8)]
         [TestCase("10,2000",10)]
+        [TestCase("//;\n2;5",7)]
+        //[TestCase("//[***]\n11***22***33", 66)]
         public void Add_Numbers_ReturnSum(string number, double expected)
         {
             double results = calculator.Add(number);
@@ -33,7 +30,7 @@ namespace Main
     {
        public double Add(string number)
        {
-            string[] numberArray = number.Split(new[] { "\n", ","}, StringSplitOptions.None);
+            string[] numberArray = number.Split(new[] { "/", ";", "\n", "," }, StringSplitOptions.None) ;
             double sum = 0;
 
             if (number.Length == 0 || Char.IsLetter(number,0))
