7bb15b669cc771e411a34a8ddfa1f96205bf7209
lto28 <to.larry92@gmail.com>
2019-09-09 22:20:47 -0700

    removed repetitive cde and broke up unit test

--- StringCalculatorTest.cs
+++ StringCalculatorTest.cs
@@ -11,24 +11,65 @@ namespace Main
         StringCalculator calculator = new StringCalculator();
 
         [TestCase("",0)]
-        [TestCase("20", 20)]
-        [TestCase("tytyt", 0)]
-        [TestCase("1,100", 101)]
-        [TestCase("5,tytyt",5)]
-        [TestCase("1,2,3,4,5,6,7,8,9,10,11,12",78)]
-        [TestCase("1\n2,3",6)]
-        [TestCase("2,1000,6",8)]
-        [TestCase("10,2000",10)]
-        [TestCase("//;\n2;5",7)]
-        [TestCase("//[***]\n11***22***33", 66)]
-        [TestCase("//[*][!!][r9r]\n11r9r22*33!!44", 110)]
-        public void Add_Numbers_ReturnSum(string number, double expected)
+        public void Add_EmptyString_ReturnsZero(string number, double expected)
         {
             double results = calculator.Add(number);
-
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("20", 20)]
+        [TestCase("tytyt", 0)]
+        public void Add_SingleNumber_ReturnsSum(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("1,100", 101)]
+        [TestCase("10,50", 60)]
+        [TestCase("5,tytyt",5)]
+        public void Add_MultiNumber_ReturnsSum(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("1,2,3,4,5,6,7,8,9,10,11,12",78)]
+        public void Add_ManyNumbers_ReturnsSum(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("1\n2,3",6)]
+        public void Add_Supports_NewLine(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("2,1000,6",8)]
+        [TestCase("10,1001", 10)]
+        public void Add_Ignore_GreaterThan1000(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("//;\n2;5",7)]
+        public void Add_Support_CustomChar(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("//[***]\n11***22***33", 66)]
+        public void Add_Support_CustomDelimiter(string number, double expected)
+        {
+            double results = calculator.Add(number);
+            Assert.AreEqual(expected, results);
+        }
+        [TestCase("//[*][!!][r9r]\n11r9r22*33!!44", 110)]
+        public void Add_Support_MultiDelimiter(string number, double expected)
+        {
+            double results = calculator.Add(number);
             Assert.AreEqual(expected, results);
         }
         [TestCase("1,-2","-2")]
+        [TestCase("1,-2,-3,-4","-2,-3,-4")]
         public void ThrowException_NegativeNumbers(string number, string expected)
         {
             var exception = Assert.Throws<Exception>(() => this.calculator.Add(number));
@@ -42,41 +83,22 @@ namespace Main
             string[] numberArray = number.Split(new[] { "/","***", "*", "r9r", "!", ";", "\n", "," }, StringSplitOptions.None) ;
             double sum = 0;
 
-            if (number.Length == 0 || Char.IsLetter(number,0))
+            for (int i = 0; i < numberArray.Length; i++)
             {
-                return 0;
-            }
-            else if (numberArray.Length > 1)
-            {
+                double num = 0;
+                bool valid = double.TryParse(numberArray[i], out num);
                 var negativeNumbers = new List<double>();
-                for (int i = 0; i < numberArray.Length; i++)
+                if (valid && num < 1000)
                 {
-                    double num = 0;
-                    bool valid1 = double.TryParse(numberArray[i], out num);
-                    if (valid1 && num < 1000 && num > 0)
-                    {
-                        sum += num;
+                    if (num < 0)
+                        negativeNumbers.Add(num);
+                    sum += num;
                     }
-                    else if (num < 0)
-                    {
-                        double num1 = double.Parse(numberArray[i]);
-                        
-                        if (num1 < 0)
-                        {
-                            negativeNumbers.Add(num1);
-                        }
-                        sum += num1;
-                        if (negativeNumbers.Any())
-                        {
-                            throw new Exception("Negatives not allowed: " + string.Join(",", negativeNumbers));
-                        }
-                        return sum;
-                    }
-                    
+                    if (negativeNumbers.Any())
+
+                        throw new Exception("Negatives not allowed: " + string.Join(",", negativeNumbers));
                 }
                 return sum;
-            }
-            return int.Parse(number);
         }
     }
     }
